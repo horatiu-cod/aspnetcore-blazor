ca8c1130fe151e915a7eaaf9b737807672451658
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-02-02 09:40:11 +0200

    Clean Project

--- AspNetCoreBlazorApp/AspNetCoreBlazorUI/Program.cs
+++ AspNetCoreBlazorApp/AspNetCoreBlazorUI/Program.cs
@@ -1,11 +1,11 @@
+using AspNetCoreBlazorUI.Startup_Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.ConfigureServices();
 
 
 var app = builder.Build();
---
+++ AspNetCoreBlazorApp/AspNetCoreBlazorUI/Startup Services/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace AspNetCoreBlazorUI.Startup_Services;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+
+        return builder; 
+    }
+}
